1d1d687f0f611a1f8a2a4889f626301f12d62d9b
devgl96 <georgelucas.dev@gmail.com>
2021-02-15 11:13:50 -0300

    Lesson 2 of AluraQuiz

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 // Import components from folder src
+import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 
 // const BackgroundImage = styled.div`
@@ -25,17 +29,77 @@ export const QuizContainer = styled.div`
   }
 `;
 
+export const Input = styled.input`
+  width: 100%;
+
+  background: none;
+  font-size: 0.7rem;
+  border: 2px solid ${({theme}) => theme.colors.primary};
+  border-radius: 3px;
+  margin: 1.5em 0;
+  padding: 0.75em;
+  color: #FFF;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  background: ${({theme}) => theme.colors.secondary};
+  color: ${({theme}) => theme.colors.primary};
+
+  font-size: 1em;
+  margin: 1em 0 0;
+  padding: 0.50em 1em;
+  border: 2px solid ${({theme}) => theme.colors.primary};
+  border-radius: 3px;
+
+  &:disabled {
+    background: #333;
+    color: #DDD;
+    border: 2px solid #DDD;
+  }
+`;
+
 export default function Home() {
+  const router = useRouter();
+  
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - CSS Quiz</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         {/* First Widget */}
         <Widget>
           <Widget.Header>
             <h1>CSS Quiz</h1>
           </Widget.Header>
-          <Widget.Content> 
-            <p>lorem ipsum dalet from ...</p>
+          <Widget.Content>
+            <p>Teste os seus conhecimentos sobre o universo CSS e divirta-se criando o seu AluraQuiz!</p>
+            <form onSubmit={(infosDoEvent) => {
+              infosDoEvent.preventDefault();
+              
+              router.push(`/quiz?name=${name}`);
+
+              console.log("Making a submit from react");
+            }}>
+              {/* <input 
+                onChange={(infosDoEvent) => {
+                  setName(infosDoEvent.target.value);
+                }}
+                placeholder="Diz ai seu nome"/> */}
+              <Input 
+                onChange={(infosDoEvent) => {
+                  setName(infosDoEvent.target.value);
+                }}
+                placeholder="Diz ai seu nome"/>
+              
+              <Button type="submit" disabled={name.length === 0}>
+                JOGAR
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,26 +1,28 @@
 import styled from 'styled-components';
 
 const Widget = styled.div`
-    margin-top: 24px;
-    margin-bottom: 24px;
-    border: 1px solid ${({theme}) => theme.colors.primary};
-    background-color: ${({theme}) => {
-        return theme.colors.mainBg;
-    }};
-    border-radius: 4px;
-    overflow: hidden;
+        margin-top: 24px;
+        margin-bottom: 24px;
+        border: 1px solid ${({theme}) => theme.colors.primary};
+        background-color: ${({theme}) => {
+            return theme.colors.mainBg;
+        }};
+
+        border-radius: 4px;
+        overflow: hidden;
 
     h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1;
+        margin-bottom: 0;
     }
 
     p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
+        font-family: 'Lato', sans-serif;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1;
     }
 `;
 
@@ -31,6 +33,8 @@ Widget.Header = styled.header`
     padding: 18px 32px;
     background-color: ${({theme}) => theme.colors.primary};
 
+    font-family: 'Lato', sans-serif;
+
     * {
     margin: 0;
     }
@@ -38,17 +42,19 @@ Widget.Header = styled.header`
 
 Widget.Content = styled.div`
     padding: 24px 32px 32px 32px;
+    font-family: 'Lato', sans-serif;
+
     & > *:first-child {
-    margin-top: 0;
+        margin-top: 0;
     }
 
     & > *:last-child {
-    margin-bottom: 0;
+        margin-bottom: 0;
     }
 
     ul {
-    list-style: none;
-    padding: 0;
+        list-style: none;
+        padding: 0;
     }
 `;
 
